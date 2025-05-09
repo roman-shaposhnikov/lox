d3960c8c2f87290b801cc6de4e028a2b695a81d7
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-05-09 13:09:09 +0300

    feat(cs/Scanner): implement ProfuceTokens

--- cs-lox/lox/Scanner.cs
+++ cs-lox/lox/Scanner.cs
@@ -1,7 +1,25 @@
 class Scanner(string source) {
   readonly string source = source;
+  readonly List<Token> tokens = [];
+  int startScanningIndex = 0;
+  int currentCharIndex = 0;
+  int currentScanningLine = 1;
 
   public List<Token> ProduceTokens() {
-    return [];
+    while (!CheckIsEOFReached()) {
+      // We are at the beginning of the next lexeme.
+      startScanningIndex = currentCharIndex;
+      ScanToken();
+    }
+
+    tokens.Add(new Token(TokenType.EOF, "", null, currentScanningLine));
+
+    return tokens;
   }
+
+  bool CheckIsEOFReached() {
+    return currentCharIndex >= source.Length;
+  }
+
+  void ScanToken() {}
 }
