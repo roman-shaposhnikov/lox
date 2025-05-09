85fa9572e6c977c9ad7c6d69f944042a8372a328
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-05-09 20:35:01 +0300

    feat(cs/Parser): generate Ast nodes and Visitor interface

---
+++ cs-lox/lox/Expression.cs
@@ -0,0 +1,58 @@
+// ##############################################
+// ####  This file was generated!            ####
+// ####  Do not change it manually           ####
+// ####  Check: cs-lox/tools/generateAst.sh  ####
+// ##############################################
+
+interface AstVisitor<ReturnValue> {
+  ReturnValue VisitGroupingExpression(Grouping expression);
+  ReturnValue VisitBinaryExpression(Binary expression);
+  ReturnValue VisitUnaryExpression(Unary expression);
+  ReturnValue VisitLiteralExpression(Literal expression);
+}
+
+abstract class Expression {
+  public abstract ReturnType Accept<ReturnType>(AstVisitor<ReturnType> visitor);
+}
+
+class Grouping(
+  Expression expression
+) : Expression {
+  public readonly Expression expression = expression;
+  public override ReturnType Accept<ReturnType>(AstVisitor<ReturnType> visitor) {
+    return visitor.VisitGroupingExpression(this);
+  }
+}
+
+class Binary(
+  Expression left,
+  Token oper,
+  Expression right
+) : Expression {
+  public readonly Expression left = left;
+  public readonly Token oper = oper;
+  public readonly Expression right = right;
+  public override ReturnType Accept<ReturnType>(AstVisitor<ReturnType> visitor) {
+    return visitor.VisitBinaryExpression(this);
+  }
+}
+
+class Unary(
+  Token oper,
+  Expression right
+) : Expression {
+  public readonly Token oper = oper;
+  public readonly Expression right = right;
+  public override ReturnType Accept<ReturnType>(AstVisitor<ReturnType> visitor) {
+    return visitor.VisitUnaryExpression(this);
+  }
+}
+
+class Literal(
+  object? value
+) : Expression {
+  public readonly object? value = value;
+  public override ReturnType Accept<ReturnType>(AstVisitor<ReturnType> visitor) {
+    return visitor.VisitLiteralExpression(this);
+  }
+}
