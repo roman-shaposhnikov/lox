87fff656a5478a4b72997645d1599390f2d87d6a
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-07-04 21:55:25 +0300

    wip: a virtual machine

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
 
-void initVM() {}
+static void resetStack() {
+  vm.stackTop = vm.stack;
+}
+
+void initVM() {
+  resetStack();
+}
 
 void freeVM() {}
+
+void push(Value value) {
+  *vm.stackTop = value;
+  vm.stackTop++;
+}
+
+Value pop() {
+  vm.stackTop--;
+  return *vm.stackTop;
+}
 
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
@@ -15,6 +32,10 @@ static InterpretResult run() {
     (vm.chunk->constants.values[READ_BYTE()])
 
   for (;;) {
+    #ifndef DEBUG_TRACE_EXECUTION
+      disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+    #endif
+
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
       case OP_CONSTANT: {
