6d033f6746005d268f6511011bd31b1ed3967ff3
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-09-12 22:02:55 +0300

    feat(c): vm

---
+++ clox/src/vm.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#include "common.h"
+#include "vm.h"
+
+static InterpretResult run();
+
+VM vm;
+
+void initVM() {
+}
+
+void freeVM() {}
+
+
+InterpretResult interpret(Chunk* chunk) {
+  vm.chunk = chunk;
+  vm.ip = vm.chunk->code;
+  return run();
+}
+
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+  for (;;) {
+    uint8_t instruction;
+    switch (instruction = READ_BYTE()) {
+      case OP_CONSTANT: {
+        Value constant = READ_CONSTANT();
+        printValue(constant);
+        printf("\n");
+        break;
+      }
+      case OP_RETURN: {
+        return INTERPRET_OK;
+      }
+    }
+  }
+
+#undef READ_BYTE
+#undef READ_CONSTANT
+}
