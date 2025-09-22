c29e5b7e4d010b0afa69aab891440569832d0305
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-09-22 22:52:13 +0300

    feat(c): strings

---
+++ clox/src/object.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "memory.h"
+#include "object.h"
+#include "value.h"
+#include "vm.h"
+
+static Obj* allocateObject(size_t size, ObjType type);
+static ObjString* allocateString(char* chars, int length);
+
+/*
+  If we had an ObjString for a string literal, and tried to free its character
+  array that pointed into the original source code string, bad things would happen.
+  So, for literals, we preemptively copy the characters over to the heap.
+  This way, every ObjString reliably owns its character array and can free it.
+*/
+ObjString* copyString(const char* chars, int length) {
+  char* heapChars = ALLOCATE(char, length + 1);
+  memcpy(heapChars, chars, length);
+  heapChars[length] = '\0';
+  return allocateString(heapChars, length);
+}
+
+void printObject(Value value) {
+  switch (OBJ_TYPE(value)) {
+    case OBJ_STRING: {
+      printf("%s", AS_STRING(value)->chars);
+      break;
+    }
+  }
+}
+
+#define ALLOCATE_OBJ(type, objType) \
+  (type*)allocateObject(sizeof(type), objType)
+
+static Obj* allocateObject(size_t size, ObjType type) {
+  Obj* object = (Obj*)reallocate(NULL, 0, size);
+  object->type = type;
+
+  object->next = vm.objects;
+  vm.objects = object;
+  return object;
+}
+
+static ObjString* allocateString(char* chars, int length) {
+  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+  string->length = length;
+  string->chars = chars;
+  return string;
+}
+
+ObjString* takeString(char* chars, int length) {
+  return allocateString(chars, length);
+}
--- clox/src/value.c
+++ clox/src/value.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
+#include "object.h"
 #include "memory.h"
 #include "value.h"
 
@@ -39,6 +41,10 @@ void printValue(Value value) {
       printf("%g", AS_NUMBER(value));
       break;
     }
+    case VAL_OBJ: {
+      printObject(value);
+      break;
+    }
   }
 }
 
@@ -50,6 +56,14 @@ bool valuesEqual(Value a, Value b) {
     case VAL_BOOL:   return AS_BOOL(a) == AS_BOOL(b);
     case VAL_NIL:    return true;
     case VAL_NUMBER: return AS_NUMBER(a) == AS_NUMBER(b);
+    case VAL_OBJ: {
+      ObjString* aString = AS_STRING(a);
+      ObjString* bString = AS_STRING(b);
+      if (aString->length != bString->length) {
+        return false;
+      }
+      return memcmp(aString->chars, bString->chars, aString->length) == 0;
+    }
     default:         return false; // Unreachable.
   }
 }
