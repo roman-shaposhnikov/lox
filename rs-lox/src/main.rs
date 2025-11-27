dc9effe0c0e79626fffe1071fbd28f9b1ca0bb36
roman-shaposhnikov <roman_shaposhnikov_pub@mail.ru>
2025-11-27 23:21:00 +0300

    feat(rs): tokenize script from file

--- rs-lox/src/main.rs
+++ rs-lox/src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+use std::env::Args;
+use std::process;
+use rs_lox::scanner::Scanner;
+use rs_lox::scanner::token::{ Token };
+use rs_lox::script::Script;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args();
+    let config = Config::build(args).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(64);
+    });
+    let script = Script::build(config.path).leak();
+    let scanner = Scanner::new(&script);
+    let tokens: Vec<Token> = scanner.collect();
+    println!("{:?}", tokens);
+}
+
+struct Config {
+    path: String,
+}
+
+impl Config {
+    fn build(args: Args) -> Result<Self, &'static str> {
+        let args: Vec<String> = args.skip(1).collect();
+        match args.len() {
+            0 => Err("No path provided"),
+            1 => Ok(Config { path: args[0].clone() }),
+            _ => Err("Too much arguments"),
+        }
+    }
 }
---
+++ rs-lox/src/script.rs
@@ -0,0 +1,22 @@
+use std::{ fs, ops::Deref };
+
+pub struct Script(String);
+
+impl Script {
+    pub fn build(path: String) -> Self {
+        let content = fs::read_to_string(path).unwrap();
+        Self(content)
+    }
+
+    pub fn leak(self) -> &'static String {
+        Box::leak(Box::new(self))
+    }
+}
+
+impl Deref for Script {
+    type Target = String;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
